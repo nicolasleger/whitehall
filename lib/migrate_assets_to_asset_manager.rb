30018d745bf4974e0d97a86bc73d873299c406cc
Chris Roos <chris@seagul.co.uk>
2017-11-01 16:54:52 +0000

    Explicitly raise GdsApi::BaseError

--- lib/migrate_assets_to_asset_manager.rb
+++ lib/migrate_assets_to_asset_manager.rb
@@ -15,7 +15,8 @@ class MigrateAssetsToAssetManager
     def perform(file_path)
       # raise "This is a test. Please ignore."
       # raise GdsApi::HTTPUnauthorized.new(999)
-      raise StandardError.new('Test is a test. Please ignore.')
+      # raise StandardError.new('Test is a test. Please ignore.')
+      raise GdsApi::BaseError.new('This is a test. Please ignore.')
       file = OrganisationLogoFile.open(file_path)
       create_whitehall_asset(file) unless asset_exists?(file)
     end
