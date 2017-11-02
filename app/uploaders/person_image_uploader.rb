9723d9e1e042ed56709fb3bf5256ec25070a94ae
Chris Roos <chris@seagul.co.uk>
2017-11-02 16:05:27 +0000

    Use AssetManagerAndQuarantinedFileStorage for PersonImageUploader

--- app/uploaders/person_image_uploader.rb
+++ app/uploaders/person_image_uploader.rb
@@ -1,2 +1,3 @@
 class PersonImageUploader < ImageUploader
+  storage :asset_manager_and_quarantined_file_storage
 end
--- test/unit/person_image_uploader_test.rb
+++ test/unit/person_image_uploader_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PersonImageUploaderTest < ActiveSupport::TestCase
-  test 'uses the quarantined file storage engine' do
-    assert_equal Whitehall::QuarantinedFileStorage, PersonImageUploader.storage
+  test 'uses the asset manager and quarantined file storage engine' do
+    assert_equal Whitehall::AssetManagerAndQuarantinedFileStorage, PersonImageUploader.storage
   end
 end
