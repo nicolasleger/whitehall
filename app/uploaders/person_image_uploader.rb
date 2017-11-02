8ad179d8b2e17ca586299bdc3ea26ed0e0807f3c
Chris Roos <chris@seagul.co.uk>
2017-11-02 17:44:28 +0000

    Duplicate the ImageUploader for PersonImageUploader
    Unfortunately, inheriting didn't work as expected because the versions
    were only created in the quarantined file store and not in asset
    manager. I presume this is because the versions are defined in
    ImageUploader and that uploader uses the quarantined file store.
    
    It might be possible to remove some of this duplication but at least
    this allows me to make some more progress.

--- app/uploaders/person_image_uploader.rb
+++ app/uploaders/person_image_uploader.rb
@@ -1,3 +1,37 @@
-class PersonImageUploader < ImageUploader
+class PersonImageUploader < WhitehallUploader
   storage :asset_manager_and_quarantined_file_storage
+
+  include CarrierWave::MiniMagick
+
+  configure do |config|
+    config.remove_previously_stored_files_after_update = false
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png svg)
+  end
+
+  version :s960, if: :bitmap? do
+    process resize_to_fill: [960, 640]
+  end
+  version :s712, from_version: :s960, if: :bitmap? do
+    process resize_to_fill: [712, 480]
+  end
+  version :s630, from_version: :s712, if: :bitmap? do
+    process resize_to_fill: [630, 420]
+  end
+  version :s465, from_version: :s630, if: :bitmap? do
+    process resize_to_fill: [465, 310]
+  end
+  version :s300, from_version: :s465, if: :bitmap? do
+    process resize_to_fill: [300, 195]
+  end
+  version :s216, from_version: :s300, if: :bitmap? do
+    process resize_to_fill: [216, 140]
+  end
+
+  def bitmap?(new_file)
+    return if new_file.nil?
+    !(new_file.content_type =~ /svg/)
+  end
 end
